8c2084c722cd495eeb51f33192997be9b9c6534f
Marcin Kunicki <masel.popowo@gmail.com>
2021-11-23 19:27:54 +0100

    Init project

---
+++ src/main/java/pl/gov/sw/acalc/AmazingCalcServerApplication.java
@@ -0,0 +1,13 @@
+package pl.gov.sw.acalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingCalcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazingCalcServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/gov/sw/acalc/AmazingCalcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gov.sw.acalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingCalcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
